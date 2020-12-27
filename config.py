d5bdade15687f4381aa61878abb156922a9194fe
napo7890 <napo789@gmail.com>
2020-12-27 11:25:59 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-EMAIL_FROM = '@gmail.com'
-EMAIL_PASSWORD = ''
-EMAIL_SERVER = ''
+EMAIL_FROM = 'YOUR GMAIL'
+EMAIL_PASSWORD = 'PASSWORD'
+EMAIL_SERVER = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_TO = '@gmail.com'
+EMAIL_TO = 'Choose any email'
 URL_SOURCE_FILE = r'C:\Users...\input.csv'
 USER_AGENT = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36)"}
 PRICES_File_PATH = r'C:\Users\...\current-prices.csv'
