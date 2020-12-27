6f20a41002f0da2e5398aeac553404be0912657d
napo7890 <napo789@gmail.com>
2020-12-27 12:21:03 +0200

    v1

--- config.py
+++ config.py
@@ -2,10 +2,8 @@ EMAIL_FROM = 'gmsisters.seo@gmail.com'
 EMAIL_PASSWORD = 'efadgltiquiqpqls'
 EMAIL_SERVER = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_TO = 'gmsisters.seo@gmail.com'  # moshe.ma-yafit@naturalint.com
-URL_SOURCE_FILE = r'C:\Users\moshe.ma-yafit\Documents\SEO\Health Check\meta_data_checker\input.csv'
+EMAIL_TO = 'gmsisters.seo@gmail.com'
+URL_SOURCE_FILE = r'C:\Users\moshe.ma-yafit\PycharmProjects\price-alert\input.csv'
 USER_AGENT = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36)"}
 PRICES_File_PATH = r'C:\Users\moshe.ma-yafit\PycharmProjects\price-alert\current-prices.csv'
 
-#to send the email, we use the parameters of the Gmail server. First, we specify the server location (or its IP) and then the port to use. If the email is from another service, then we should find the corresponding info.
-# to send the email from gmail, allow less secure apps on the Gmail account https://www.google.com/settings/security/lesssecureapps - TURN ON
