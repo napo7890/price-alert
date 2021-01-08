78319d57b7ea22e3c2d15012cd9e9e15c5627009
napo7890 <napo789@gmail.com>
2021-01-08 09:40:16 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ EMAIL_PORT = 587
 EMAIL_FROM = 'EMAIL FROM'
 EMAIL_PASSWORD = 'EMAIL PASSWORD'
 EMAIL_TO = 'EMAIL TO'
-URL_SOURCE_FILE = r'C:\Users\moshe.ma-yafit\PycharmProjects\price-alert\input.csv'
+URL_SOURCE_FILE = r'C:\....\input.csv'
 USER_AGENT = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36)"}
-PRICES_File_PATH = r'C:\Users\moshe.ma-yafit\PycharmProjects\price-alert\saved-prices.csv'
+PRICES_File_PATH = r'C:\...\price-alert\saved-prices.csv'
 
